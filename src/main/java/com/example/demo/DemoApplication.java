c93f211e748a025b44340685842036dea9a1b252
hieu6908@gmail.com <youremail@domain.com>
2022-12-20 16:22:47 +0700

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("test 124 fsdfsd fsdfsdf");
+		System.out.println("test 124 fsdfsd fsdfsdf 123213");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
